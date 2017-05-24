359504a69c5c1c0339e3fa5deaa68039e729efdb
Karns <pkarns@deloitte.com>
2017-05-24 15:18:54 -0400

    updated connection url to new mlab

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://heroku_ldmd2zww:7864n8dgcq76lqrguvrdna3tbb@ds153521.mlab.com:53521/heroku_ldmd2zww");
+mongoose.connect("mongodb://heroku_ldmd2zww:7864n8dgcq76lqrguvrdna3tbb@@ds151431.mlab.com:51431/heroku_8r3btq1p");
 var db = mongoose.connection;
 
 // Show any mongoose errors
